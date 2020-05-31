e53ed8e4cf2d6bd9dff97480e6953c6a6e8ee78f
Victor Pedrosa <victor@qmedia.com.br>
2020-05-30 21:33:31 -0300

    First Commit

---
+++ android/app/src/main/kotlin/com/victor/lodjinha/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.victor.lodjinha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:lodjinha/src/app.dart';
+
+void main() {
+  runApp(App());
+}
